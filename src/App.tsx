bf6e9c2b1d5a49d1e3f121603b61c8bc432477c5
Taylor Seamans <taylor.seamans@yahoo.com>
2023-04-17 22:03:40 -0700

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Grommet, Heading, Page, PageContent } from "grommet-exp";
+
+function App() {
+  return (
+    <Grommet>
+      <Page>
+        <PageContent>
+          <Heading level={1}>Hello world.</Heading>
+        </PageContent>
+      </Page>
+    </Grommet>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import image from "@rollup/plugin-image";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [image(), react(), vanillaExtractPlugin()],
+});
